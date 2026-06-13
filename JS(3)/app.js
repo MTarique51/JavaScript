776618599ab0c4d1ffb0b8627f9ddf682ec6a20a
MTarique51 <mohdtarique7223@gmail.com>
2026-06-13 11:08:03 -1200

    Operators in JS

---
+++ JS(3)/app.js
@@ -0,0 +1,13 @@
+// ARITHMETIC OPERATORS
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// UNARY OPERATORS
+console.log(a++);     //10
+console.log(++a);     //12

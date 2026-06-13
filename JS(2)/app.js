1754b2e95139d603561bdec94339e84869e5426d
MTarique51 <mohdtarique7223@gmail.com>
2026-06-13 10:46:16 -1200

    Template Literals

---
+++ JS(2)/app.js
@@ -0,0 +1,9 @@
+let penPrice = 10;
+let pencilPrice = 7;
+
+// USING CONCATINATION PROPERTY
+let output = "The total price : " +(penPrice  +  pencilPrice)+ " Rupees.";
+console.log(output);
+
+// USING BACK TICK(``) PROPERTY
+console.log(`The total price is : ${penPrice+pencilPrice} Rupees.`);

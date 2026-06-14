1998be9105bbdb7953d4e6b0f6bc767272023a5f
MTarique51 <mohdtarique7223@gmail.com>
2026-06-14 11:11:44 -1200

    Operators in JS

--- JS(3)/app.js
+++ JS(3)/app.js
@@ -1,4 +1,5 @@
 // ARITHMETIC OPERATORS
+console.log("ARITHMETIC OPERATORS");
 let a = 10;
 let b = 5;
 console.log(a+b);
@@ -9,5 +10,33 @@ console.log(a%b);
 console.log(a**b);
 
 // UNARY OPERATORS
+console.log("UNARY OPERATORS");
 console.log(a++);     //10  (post increment)
 console.log(++a);     //12  (pre increment)
+
+// ASSIGNMENT OPERATOR
+console.log("ASSIGNMENT OPERATOR");
+let c = 100;
+let d = 25;
+d= c;
+console.log(d)  // 100
+
+// COMPARISON OPERATOR
+console.log( "COMPARISON OPERATOR");
+let age = 18;
+console.log("for number");
+console.log(age>18);   // false
+console.log(age>=18);  // true
+console.log(age<=18);  // true
+console.log(age<18);   // false 
+console.log(age==18);  // true
+console.log(age!=18);  // false
+
+console.log("check for == and ===");
+console.log('123' == 123);     // true (this will check only the value)
+console.log('123' === 123);    // false (this wiill check value and type)
+
+console.log("for non number");
+console.log('A'>'a');  //false
+console.log('a'>'A');  //true
+console.log('P'>'p');  //false

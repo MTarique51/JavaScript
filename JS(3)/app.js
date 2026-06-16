0bd29283774b113099b6698044a4f1bed7429f33
MTarique51 <mohdtarique7223@gmail.com>
2026-06-15 20:11:25 -1200

    .

--- JS(3)/app.js
+++ JS(3)/app.js
@@ -33,8 +33,8 @@ console.log(age==18);  // true
 console.log(age!=18);  // false
 
 console.log("check for == and ===");
-console.log('123' == 123);     // true (this will check only the value)
-console.log('123' === 123);    // false (this wiill check value and type)
+console.log('1234' == 1234);     // true (this will check only the value)
+console.log('1234' === 1234);    // false (this wiill check value and type)
 
 console.log("for non number");
 console.log('A'>'a');  //false

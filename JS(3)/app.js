634da7347eb571203f7a6d1bc68a7a06ffe59eb3
MTarique51 <mohdtarique7223@gmail.com>
2026-06-23 10:06:43 -1200

    .

--- JS(3)/app.js
+++ JS(3)/app.js
@@ -11,8 +11,8 @@ console.log(a**b);
 
 // UNARY OPERATORS
 console.log("UNARY OPERATORS");
-console.log(a++);     //10  (post increment)
-console.log(++a);     //12  (pre increment)
+console.log(a++);     //10  (Post increment)
+console.log(++a);     //12  (Pre increment)
 
 // ASSIGNMENT OPERATOR
 console.log("ASSIGNMENT OPERATOR");

d4fa6875b6391a76038e2a29bb6e10048d17bf5e
MTarique51 <mohdtarique7223@gmail.com>
2026-06-24 17:19:34 -1200

    Conditional Statements

--- JS(3)/app.js
+++ JS(3)/app.js
@@ -40,3 +40,68 @@ console.log("for non number:");
 console.log('A'>'a');  //false
 console.log('A'<'a');  //true
 console.log('B'>'b');  //false
+
+                                          
+
+                                        // CONDITIONAL  STATEMENTS  //
+
+
+  // if (Statement)                                      
+
+console.log("Before my if statement");
+let Age = 23;
+if(Age >=18 ){
+    console.log("You can vote");
+    console.log("You can drive");
+}
+if(Age > 20 ){
+    console.log("You are in your 20's");
+}
+console.log("After my if statement");
+
+let firstName = "Tarique";
+if(firstName == "Tarique"){
+    console.log(`Welcome ${firstName}`);
+}
+
+
+// else if (Statement)
+
+if(Age >=18 ){
+    console.log("can vote");
+    console.log("can drive");
+}
+else if(Age <18 ){
+    console.log("can't vote");
+    console.log("can't drive");
+}
+
+// if , else if , else (Statements)
+let color = "";
+if(color === "red"){
+    console.log("Please Stop!");
+}
+else if(color === "yellow"){
+    console.log("Please Slow down!");
+}
+else if(color === "green"){
+    console.log("Please Go!");
+}
+else{
+    console.log("Traffic light is Broken");
+}
+
+// Nested if else (Statement)
+let marks = 99;
+if(marks >= 33){
+    console.log("Pass");
+    if(marks >= 80){
+    console.log("Grade : A+");
+}
+else{
+    console.log("Grade : A");
+}
+}
+else{
+    console.log("Better Luck Next time!");
+}

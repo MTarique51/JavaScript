6423bede19dcde7d60cbe5d8af349157463b0615
MTarique51 <mohdtarique7223@gmail.com>
2026-06-25 11:13:15 -1200

    String Methods

---
+++ JS(4)/app.js
@@ -0,0 +1,23 @@
+                                               // Trim Method //
+
+let msg = "    Hello    ";
+console.log(msg);
+console.log(msg.trim());
+
+                                              // toLowerCase & toUpperCase //
+
+let name = "Tarique";
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+
+                                              // String Methods with Arguments{indexOf("")} //
+
+let Str = "ILoveCoding";
+console.log(Str.indexOf("Love"));
+console.log(Str.indexOf("love"));
+console.log(Str.indexOf("o"));
+console.log(Str.indexOf("i"));
+console.log(Str.indexOf("Z"));
+
+                                              // Method Chaining //
+

1ee11521ae2aac951784fbdf5c6892cbedac52c2
MTarique51 <mohdtarique7223@gmail.com>
2026-07-25 18:27:05 +0530

    Activity using Query Strings

--- JS(14)/app.js
+++ JS(14)/app.js
@@ -1,24 +1,27 @@
-let btn = document.querySelector("button");
-let url2 = "https://dog.ceo/api/breeds/image/random";
+// let btn = document.querySelector("button");
 
-btn.addEventListener("click", async() => {
-    let link = await getImage();
 
-    let img = document.querySelector("#result");
-    img.setAttribute("src" , link);
-    console.log(link);
-});
+// let url2 = "https://dog.ceo/api/breeds/image/random";
 
-async function getImage() {
-    try {
-        let res = await axios.get(url2);
-        return res.data.message;
-    }
-    catch (e) {
-        console.log("error - ", e);
-        return "No Image found";
-    }
-}
+// btn.addEventListener("click", async() => {
+//     let link = await getImage();
+
+//     let img = document.querySelector("#result");
+//     img.setAttribute("src" , link);
+//     console.log(link);
+// });
+
+// async function getImage() {
+//     try {
+//         let res = await axios.get(url2);
+//         return res.data.message;
+//     }
+//     catch (e) {
+//         console.log("error - ", e);
+//         return "No Image found";
+//     }
+// }
+
 
 // btn.addEventListener("click", async() => {
 //    let fact = await getFact();
@@ -39,3 +42,47 @@ async function getImage() {
 //         return "No fact found";
 //     }
 // }
+
+
+//Sending Headers with API requests //
+// const url = "https://icanhazdadjoke.com";
+
+// async function getJokes() {
+//     try {
+//         const config = {headers: {Accept:"application/json"} };
+//         let res = await axios.get(url , config);
+//         console.log(res.data);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+
+
+// Activity using Query Strings//
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async() => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let collArr  = await getColleges(country);
+    show(collArr);
+});
+
+function show(collArr) {
+    for(coll of collArr) {
+        console.log(coll.name);    // What requires//
+    }
+}
+
+async function getColleges(country) {
+    try {
+        let res = await axios.get(url+country);
+        return res.data;
+    }
+    catch (e){
+        console.log("error : ", e);
+        return [];
+    }
+}
